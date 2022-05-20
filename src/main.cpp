74170719cd0e6d73cd2650062f3a22233a71b91b
Alan-Company <2424233600@qq.com>
2022-05-20 16:41:31 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,6 @@
 #include "AboutBack.hpp"
 #include "AboutSimulation.hpp"
 using namespace std;
-int dp[35][35] = {0};
-bool isMatch(string s, string p)
-{
-    int len1 = s.length();
-    int len2 = p.length();
-    dp[0][0] = 1;
-    for (int i = 0; i <= len1; i++)
-    {
-        for (int j = 1; j <= len2; j++)
-        {
-            if (j >= 2 && p[j - 1] == '*')
-            {
-                if (i > 0 && p[j - 2] == s[i - 1] || p[j - 2] == '.')
-                {
-                    dp[i][j] |= dp[i - 1][j];
-                }
-                dp[i][j] |= dp[i][j - 2];
-            }
-            else if (p[j - 1] == s[i - 1] || p[j - 1] == '.')
-            {
-                if (i > 0)
-                    dp[i][j] = dp[i - 1][j - 1];
-            }
-        }
-    }
-    return dp[len1][len2] == 1;
-}
 
 int main()
 {
